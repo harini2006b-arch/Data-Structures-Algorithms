8ab4af9c420441abca795a5ee85e1a787350fda9
Harini B <harini2006b@gmail.com>
2026-07-01 20:38:11 +0530

    Time: 255 ms (5.15%) | Memory: 47.6 MB (30.12%) - LeetSync

---
+++ 268-missing-number/missing-number.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        int ans=0;
+        for(int i=0;i<=nums.Length;i++){
+            if(!nums.Contains(i)){
+                return i;
+                
+            }
+        }
+        return ans;
+        
+    }
+}
