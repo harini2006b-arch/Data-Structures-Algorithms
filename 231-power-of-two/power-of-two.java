55d32039cbc903a97d965bf212945a596fe5fbb3
Harini B <harini2006b@gmail.com>
2026-07-21 20:22:11 +0530

    Time: 1 ms (96.62%) | Memory: 42.6 MB (55.76%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,9 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        // while(n%2 == 0){
-        //     n/=10;
-        // }
-        // return n==1;
+        
         return (n > 0) && (n & (n-1)) == 0;
     }
 }
