525c2f52a6483b7246a077c8dd3a301bdf084837
Harini B <harini2006b@gmail.com>
2026-07-25 07:19:01 +0530

    Time: 1 ms (3.06%) | Memory: 43.7 MB (8.93%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        ArrayList<Integer>al=new ArrayList<>();
+        for(int num:nums){
+            al.add(num);
+        }
+        while(al.contains(val)){
+            al.remove(Integer.valueOf(val));
+        }
+        for(int i=0;i<al.size();i++){
+            nums[i]=al.get(i);
+        }
+        return al.size();
+    }
+}
