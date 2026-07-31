6ae79d88fc4265737dcdf353a113c11a34274203
Harini B <harini2006b@gmail.com>
2026-07-31 10:51:06 +0530

    Time: 30 ms (74.84%) | Memory: 146.9 MB (50.32%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,34 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        int ri = 0; 
+        
+        Deque<Integer> q = new ArrayDeque<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!q.isEmpty() && q.peek() == i - k) {
+                q.poll();
+            }
+            
+            while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) {
+                q.pollLast();
+            }
+            
+            q.offer(i);
+            
+            if (i >= k - 1) {
+                res[ri++] = nums[q.peek()];
+            }
+        }
+        
+        return res;
+    }
+}
