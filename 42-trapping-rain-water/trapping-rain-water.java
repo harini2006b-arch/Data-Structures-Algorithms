2c7bcef71a638d32d2a6b2927f87192136962831
Harini B <harini2006b@gmail.com>
2026-07-31 13:20:22 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (82.29%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] arr) {
+        if (arr == null || arr.length == 0) return 0;
+        int l = 0, r = arr.length - 1;
+        int tcap = 0;
+        int lmax = arr[0], rmax = arr[arr.length - 1];
+        while (l < r) {
+            if (lmax < rmax) {
+                l++;
+                lmax = Math.max(lmax, arr[l]);
+                tcap += (lmax - arr[l]);
+            } else {
+                r--;
+                rmax = Math.max(rmax, arr[r]);
+                tcap += (rmax - arr[r]);
+            }
+        }
+        return tcap;
+    }
+}
