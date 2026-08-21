ad78cf98568ec8636edbafccf09c761213250386
Harini B <harini2006b@gmail.com>
2026-08-21 07:01:30 +0530

    Time: 2 ms (57.66%) | Memory: 46.7 MB (66.27%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -4,11 +4,10 @@ class Solution {
         List<Integer>arr2=new ArrayList<>();
         arr1.add(nums[0]);
         arr2.add(nums[1]);
-        
         for(int i=2;i<nums.length;i++){
-            int lastof1=arr1.get(arr1.size()-1);
-            int lastof2=arr2.get(arr2.size()-1);
-            if(lastof1>lastof2){
+            int one=arr1.get(arr1.size()-1);
+            int two=arr2.get(arr2.size()-1);
+            if(one>two){
                 arr1.add(nums[i]);
             }
             else{
@@ -17,12 +16,16 @@ class Solution {
         }
         int res[]=new int[nums.length];
         int index=0;
-        for(int num:arr1){
+        for(int num : arr1){
             res[index++]=num;
         }
-        for(int num:arr2){
+        for(int num : arr2){
             res[index++]=num;
         }
         return res;
+
+        
+        
+        
     }
 }
