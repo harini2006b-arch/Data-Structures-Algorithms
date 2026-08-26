9ee1b3ae444bf878da8e5975a9c020c74c9da203
Harini B <harini2006b@gmail.com>
2026-08-26 21:18:06 +0530

    Time: 35 ms (40.91%) | Memory: 100.1 MB (43.60%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -3,10 +3,12 @@ class MinStack {
     Stack<Integer> stack;
     Stack<Integer> minStack;
 
+
     public MinStack() {
         stack = new Stack<>();
         minStack = new Stack<>();
     }
+
 
     public void push(int val) {
 
