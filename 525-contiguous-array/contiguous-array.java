083cb8229f4e225f55b35e79b077b30bb4cf9fb0
Harini B <harini2006b@gmail.com>
2026-09-04 21:30:32 +0530

    Time: 25 ms (19.53%) | Memory: 65.5 MB (58.93%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int balance = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                balance--;
+            } else {
+                balance++;
+            }
+            if (map.containsKey(balance)) {
+                max = Math.max(max, i - map.get(balance));
+            } else {
+                map.put(balance, i);
+            }
+        }
+        return max;
+    }
+}
